2013d71f73b229c87045f1c5b5eae7febfd026bf
Bibek-Poudel8 <bibekeight8@gmail.com>
2025-10-11 15:56:24 +0545

    Fix

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar"
 import Home from "./Home"
 import AboutMe from "./AboutMe"
-import Footer from "./footer"
+import Footer from "./Footer"
 
 
 function Layout() {
