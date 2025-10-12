702f5a7d825d79fca5936ab8eb6d12d6da90af5d
Bibek-Poudel8 <bibekeight8@gmail.com>
2025-10-12 20:18:33 +0545

    Test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -149,7 +149,7 @@ export default function Navbar() {
               duration={500}
               spy={true}             // track scroll position for activeClass toggle
               activeClass="bg-blue-500 text-white"
-              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer "
+              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer active:text-white active:bg-blue-500"
               onClick={() => setIsOpen(false)} // close menu on click
             >
               Home
