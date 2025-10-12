6bad7ee20ed9635c4822e97cac73ca656d9b3eba
Bibek-Poudel8 <bibekeight8@gmail.com>
2025-10-12 17:23:45 +0545

    Fixed previous not working issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -147,10 +147,8 @@ export default function Navbar() {
               offset={-80}           // adjust based on your navbar height
               duration={500}
               spy={true}             // track scroll position for activeClass toggle
-              tabIndex={0}
               activeClass="bg-blue-500 text-white"
-              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer active:text-white active:bg-blue-500 
-             focus:text-white focus:bg-blue-500"
+              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer "
               onClick={() => setIsOpen(false)} // close menu on click
             >
               Home
@@ -167,8 +165,7 @@ export default function Navbar() {
               duration={500}
               spy={true}             // track scroll position for activeClass toggle
               activeClass="bg-blue-500 text-white"
-              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer active:text-white active:bg-blue-500 
-             focus:text-white focus:bg-blue-500"
+              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer "
               onClick={() => setIsOpen(false)} // close menu on click
             >
               AboutMe
@@ -183,8 +180,7 @@ export default function Navbar() {
               duration={500}
               spy={true}             // track scroll position for activeClass toggle
               activeClass="bg-blue-500 text-white"
-              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer active:text-white active:bg-blue-500 
-             focus:text-white focus:bg-blue-500"
+              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer "
               onClick={() => setIsOpen(false)} // close menu on click
             >
               Skills
@@ -199,8 +195,7 @@ export default function Navbar() {
               duration={500}
               spy={true}             // track scroll position for activeClass toggle
               activeClass="bg-blue-500 text-white"
-              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer active:text-white active:bg-blue-500 
-             focus:text-white focus:bg-blue-500"
+              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer"
               onClick={() => setIsOpen(false)} // close menu on click
             >
               Projects
@@ -215,8 +210,7 @@ export default function Navbar() {
               duration={500}
               spy={true}             // track scroll position for activeClass toggle
               activeClass="bg-blue-500 text-white"
-              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer active:text-white active:bg-blue-500 
-             focus:text-white focus:bg-blue-500"
+              className="block text-base px-4 py-2 w-full text-center transition rounded-full hover:text-white hover:bg-blue-500 text-black cursor-pointer "
               onClick={() => setIsOpen(false)} // close menu on click
             >
               ContactMe
